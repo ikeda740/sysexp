52962b26fb0bf7115bc6d38f5d0c01147690f57b
Tetsushi IKEDA <ikeda@hiroshima-cu.ac.jp>
2022-03-24 16:32:29 +0900

    Modify rosbridge_tcp to rosbridge_websocket

--- script/robot_controller02.py
+++ script/robot_controller02.py
@@ -1,52 +1,59 @@
-# rosbridge_tcp json client
-import socket
-import json
-import sys, time
+# -*- coding:utf8 -*-
+# rosbridge_websocket client
 
-def make_message(twist, topic_name):
-    return json.dumps(dict(op='publish',
-                        topic=topic_name,
-                        msg=twist))
+import time
+import sys
+from roslibpy import Message, Ros, Topic
 
-def make_twist(x_lin, y_lin, z_lin, x_ang, y_ang, z_ang):
-    return dict(linear=dict(x=x_lin, y=y_lin, z=z_lin),
-                angular=dict(x=x_ang, y=y_ang, z=z_ang))
+def mainloop( host, port, topic_name ):
+    # roscoreを実行しているサーバーへ接続
+    ros_client = Ros( host, port )
 
-def mainloop( topic_name ):
-    # right, up, left, down 
+    # right, up, left, down
     xy_command_list = [[0.5, 0], [0.0, 0.5], [-0.5, 0],  [0, -0.5] ]
     n_command = len(xy_command_list)
+    repeat_times = 4 # repeat the same command to move farther
 
-    repeat_times = 6 # repeat the same command to move farther
-    i = 0
-    while True:
-        for j in range(repeat_times):
-            # set command
-            linear_x = xy_command_list[i][0]
-            linear_y = xy_command_list[i][1]
+    # Publishするtopicを指定
+    publisher = Topic(ros_client, topic_name, 'geometry_msgs/Twist')
+    def start_sending():
+        i = 0
+        while True:
+            if not ros_client.is_connected:
+                break
 
-            twist = make_twist(linear_x, linear_y, 0.0, 0.0, 0.0, 0.0)            
-            message = make_message(twist, topic_name)
+            for j in range(repeat_times):
+                # set command
+                linear_x = xy_command_list[i][0]
+                linear_y = xy_command_list[i][1]        
 
-            sock.send(message.encode('utf-8'))
-            time.sleep(0.5) # 2 Hz ?
+                # 送信するTwistメッセージの内容
+                publisher.publish(Message({
+                    'linear': {
+                        'x': linear_x,
+                        'y': linear_y,
+                        'z': 0
+                    },
+                    'angular': {
+                        'x': 0,
+                        'y': 0,
+                        'z': 0
+                    }
+                }))
+                time.sleep(1) # 2 Hz ?
 
-        i = ( i + 1 ) % n_command
-    
-    # stop robot
-    twist = make_twist(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    message = make_message(twist, topic_name)
-    sock.send(message)
-    
-    sock.close()
+            i = (i+1) % n_command
+
+        publisher.unadvertise()
+    # Publish開始
+    ros_client.on_ready(start_sending, run_in_thread=True)
+    ros_client.run_forever()
 
 if __name__ == '__main__':
     topic_name = sys.argv[1]
 
-    host = '192.168.1.107'  # IP address (Jetson)
+    #host = '192.168.100.40'
+    host = '192.168.1.107'
     port = 9090
 
-    sock = socket.socket()
-    sock.connect((host, port))
-
-    mainloop( topic_name )
+    mainloop( host, port, topic_name )
